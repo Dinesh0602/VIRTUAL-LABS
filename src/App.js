abf3319008f3ee1d4998d0f8c4e34b76bb41f03d
Dinesh Buruboyina <buruboyinadinesh@gmail.com>
2024-02-14 14:20:26 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import logo from './logo.png'
 
 function App() {
   return (
     <div className="App">
-     {/* <h1>Hello</h1> */}
+
+<div id='nav'>
+    <img src={logo} id="logo"  />
+    <span>IIT(ISM) Dhanbad</span>
+    <br/>
+    <span>Virtual Labs</span>
+
+ <nav>   
+   <ul class="nav">
+      <li><a href="personaltraining.html">HOME</a></li>
+      <li><a href="indexdesktop.html">ABOUT US</a></li>
+      <li><a href="apps.html">LABS</a></li>
+      <li><a href="contact.html">CONTACT</a></li>
+   </ul>
+</nav>
+  <div class="clear"></div>
+  </div>
     </div>
   );
 }
